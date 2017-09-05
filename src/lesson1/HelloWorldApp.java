ea33e632fd0d04b28888a3e2f265b0c0129843a9
norjin <norjoo07@gmail.com>
2017-09-05 11:53:32 -0500

    Testing git with eclipse

---
+++ src/lesson1/HelloWorldApp.java
@@ -0,0 +1,24 @@
+package lesson1;
+/**
+ * @version 0.0.1
+ * @author 985999
+ *
+ */
+public class HelloWorldApp {
+	/**
+	 * 
+	 * @param args string
+	 * @return null
+	 */
+	public static void main(String[] args) {
+//		System.out.println("Hello\n WOOORld");
+//		System.out.println('\u4b01');
+//		char cc = 'X';
+//		int num = (int) cc;
+//		String stt = Integer.toOctalString(num);
+		for(String strr: args) {
+			System.out.println(strr);
+		}
+		
+	}
+}
